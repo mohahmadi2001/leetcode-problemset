a17a74c4a1b16458c70c157a30a774cb0723b26d
mm.ahmadi <mm.ahmadi@payampardaz.com>
2025-04-09 09:59:12 +0330

    palindrom problem

--- python/tow_sum.py
+++
@@ -1,9 +0,0 @@
-def two_sum(nums: [int], target: int) -> [int]:
-    pre = {}
-
-    for i , n in enumerate(nums):
-        diff = target - n
-        if diff in pre:
-            return [pre[diff], i]
-        pre[n] = i
-
